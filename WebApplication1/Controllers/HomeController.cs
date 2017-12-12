32fa0079d68cccfe3aa957054de41ecb1a45652a
DESKTOP-3A4BHLQ\junic <junicolas@gmail.com>
2017-12-12 21:22:13 +0100

    rrr

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -18,6 +18,8 @@ namespace WebApplication1.Controllers
         {
             ViewBag.Message = "Your application description page.";
 
+            
+            
             return View();
         }
 
